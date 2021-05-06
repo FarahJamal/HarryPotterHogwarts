3a632e194971e9067ac08267018ea4a34f6f736f
farahsarese98 <>
2021-05-06 11:51:11 +0000

    adding new pages to be more clear

---
+++ JS.js
@@ -0,0 +1,12 @@
+  
+  var x=confirm("do you want a Pics about Hogwarts?");
+
+  if(x==true){
+      location.replace("./secondPage/index.html")
+      }
+  else{
+    var y=confirm("do you want a potter head quiz?")
+    location.replace("./HarryPotterQuiz/index.html");
+
+  }
+
